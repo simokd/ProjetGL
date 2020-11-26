dbd5030328935b7bf6a09a75b49f227b75676be2
ilyasse7 <61146490+ilyasse7@users.noreply.github.com>
2020-11-26 14:38:57 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define taille 100
+typedef struct _structure {
+    char* nom;
+    char* tel;
+    struct _structure* suivant;
+}structure;
+
+int Index(char *name)
+{
+    int i = 0, nombre = 0;
+
+    for (i = 0 ; name[i] != '\0' ; i++)
+    {
+        nombre += name[i];
+    }
+    nombre %= taille;
+
+    return nombre;
+}
+void ajouterItem(char* nom, char* tel, struct _structure HashTable[taille])
+{
+
+    int index = Index(nom);
+    if (HashTable[index].nom == "vide" && HashTable[index].tel == "vide")
+    {
+        HashTable[index].nom = nom;
+        HashTable[index].tel = tel;
+    }
+    else
+    {
+        structure *elements = HashTable[index].suivant;
+        structure precedent=HashTable[index];
+
+        while (elements != NULL)
+        {
+            precedent= *elements;
+            elements = elements->suivant;
+        }
+
+        elements->nom = nom;
+       elements->tel = tel;
+        elements->suivant = NULL;
+        precedent.suivant=elements;
+    }
+   printf("votre enregeistrement est bien effectue a l'indexe :%d \n",index);
+}
+
+int main(int argc, char *argv[])
+{
+structure tab[taille];
+
+
+     for (int i = 0; i<taille; i++){
+        tab[i].nom = "vide";
+        tab[i].tel = "vide";
+        tab[i].suivant = NULL;
+     }
+    ajouterItem("ilyas", "063476671",tab);
+    nombreItems(46,tab);
+    afficherItemsDansIndex(46,tab);
+
+
+
+}
