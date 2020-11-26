bde421fb935037d9744f7e58802a43165435e261
zakariaikrimah <75026498+zakariaikrimah@users.noreply.github.com>
2020-11-26 16:34:14 +0100

    Update GL2.c

--- GL2.c
+++ GL2.c
@@ -2,23 +2,18 @@
  
 int main(){
 
-int i=0,j=0,k=0,N;
+int i=0,k=0,N;
 char s[N] ;
 printf("enter la taille de votre chainne de caracteres ");
 scanf("%d",&N);
 printf("enter votre chaine de caractres   : \n ");
-scanf("%S", s);
-for (i=0;i<N-1;i++){
-    for(j=i+1;j<N;j++){
-        if(s[i]==s[j]){
-                     k=1;}break;
-        
+scanf("%s", s);
+while(s[i]!='\0'&& k==0){
+if(s[i]==s[i+1]){printf(" ",s[i]);
+k=1;}
+else i++;
 
-    }
-
-if (i==N-1 || k==1){printf(" ",s[i]);}break;
-
-}
+}if (i==N-1){prinf("NULL\n");}
 
 
 
