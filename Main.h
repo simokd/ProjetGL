5843ba3afe67443f9b2b5dd16a98a3a60236b65d
Maryame-L <58733484+Maryame-L@users.noreply.github.com>
2020-11-27 00:36:45 +0100

    Update Main.h

--- Main.h
+++ Main.h
@@ -8,6 +8,7 @@ typedef struct _structure {
     struct _structure* suivant;
 }structure;
 
+int Index(char *name);
 void ajouterItem(char* nom, char* tel, struct _structure HashTable[taille]);
 void afficherItemsDansIndex(int index, structure * HashTable[tailleTableau]);
 void chercherItem(char* nom, structure * HashTable[tailleTableau]);
