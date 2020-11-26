416433abb16ec889337a142a0b298e2323b562c6
Maryame-L <58733484+Maryame-L@users.noreply.github.com>
2020-11-27 00:57:26 +0100

    Update nombreItems.c

--- nombreItems.c
+++ nombreItems.c
@@ -10,7 +10,7 @@ int nombreItems(int index,struct _structure HashTable[taille]){
     printf("le nombre des items dans la position %d est %d \n",index, count) ;
     return count ;
 }
-void afficherItemsDansIndex(int index,struct _structure HashTable[taille]){
+/*void afficherItemsDansIndex(int index,struct _structure HashTable[taille]){
 
     printf( "Le nom est :%s ", HashTable[index].nom ) ;
     printf("le numero de telephone est : %s \n" , HashTable[index].tel ) ;
@@ -21,4 +21,4 @@ void afficherItemsDansIndex(int index,struct _structure HashTable[taille]){
         printf( "le numero de telephone est :%s \n" , elements->tel ) ;
         elements = elements->suivant;
     }
-}
+}*/
