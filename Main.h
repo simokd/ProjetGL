5d607093a5baa499a21455657a304c1939749b95
Maryame-L <58733484+Maryame-L@users.noreply.github.com>
2020-11-27 00:02:51 +0100

    Update Main.h

--- Main.h
+++ Main.h
@@ -1,7 +1,9 @@
 #ifndef Main_H
 #define Main_H
 
+void ajouterItem(char* nom, char* tel, struct _structure HashTable[taille]);
 void afficherItemsDansIndex(int index, structure * HashTable[tailleTableau]);
 void chercherItem(char* nom, structure * HashTable[tailleTableau]);
+
 
 #endif  
