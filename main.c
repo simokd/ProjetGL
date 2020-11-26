1e34c5d84cbac07af52714b247a8f4a282f394d6
Maryame-L <58733484+Maryame-L@users.noreply.github.com>
2020-11-27 00:13:04 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "Main.h"
+
 int remove_item(hashtable_t* ht, char* k) {
     unsigned int slot = hash_fct(k) % ht->size;
-    hash_item_t* item = ht->items[slot];
-    hash_item_t* prev;
+    structure* item = ht->items[slot];
+    structure* prev;
     while (item != NULL) {
         if(strcmp(item->key, k) == 0) {
             prev->next = item->next;
